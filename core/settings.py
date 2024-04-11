2560ac009e46163e805c9ba9b239eb0c78899e6e
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-04-11 14:01:08 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps_home'  # Enable the inner home (home)
+    'apps.home'  # Enable the inner home (home)
 ]
 
 MIDDLEWARE = [
