1a227643ede831fc5e11ce86b0d246a305620eff
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-04-12 16:35:24 +0700

    Update urls.py

--- apps/apec/urls.py
+++ apps/apec/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include , re_path
 from apps.home import views
-from .views import SyncUserDevice
+from .views import SyncUserDevice, GetListCompany, GetListHrmEmployees, \
+                        GetListHrmAttendanceReport
 
 urlpatterns = [
     path("syncuser/", SyncUserDevice.as_view(), name='apec_syncuser'),
+    path("listcompany/", GetListCompany.as_view(), name='v2_list'),
+    path("hrmemployees/", GetListHrmEmployees.as_view(), name='v2_list_employee'),
+    path("hrmattendances/", GetListHrmAttendanceReport.as_view(), name='v2_list_attendance'),
 ]
