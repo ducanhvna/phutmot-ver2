d57e739c2bad2de2f473e0e5fa278d606f76106a
DucAnh <ducanhvna@outlook.com>
2024-04-20 01:08:20 +0700

    Update views.py

--- apps/vantaihahai/views.py
+++ apps/vantaihahai/views.py
@@ -227,6 +227,7 @@ class Tatcachuyendi(APIView):
                             item['location_id'] = item2['location_id']
                             item['location_dest_id'] = item2['location_dest_id']
                             item['fleet_product_id'] = item2['fleet_product_id']
+                queryset['employee_id'] = employee_id
                 print(queryset)
         return Response(queryset)
         # except Exception as ex:
