84016c7867b14cd7a6fbecb7415d34df1a5921c0
DucAnh <ducanhvna@outlook.com>
2024-05-01 21:18:15 +0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,11 +8,11 @@ from django.urls import path, include  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
+    path("auth/", include("apps.authentication.urls")), # Auth routes - login / register
 
     # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
-    path('', include('apps.chat.urls')),
+    path('chat/', include('apps.chat.urls')),
     path("", include("apps.home.urls"))
 ]
