dfe878273ba801d63754cd6fe35da266c646b7e0
DucAnh <ducanhvna@outlook.com>
2024-05-10 23:51:09 +0700

    Update core.py

--- apps/vantaihahai/utils/core.py
+++ apps/vantaihahai/utils/core.py
@@ -221,6 +221,11 @@ class VanTaiHaHai():
        
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(self.url))
         drive_uid = common.authenticate(self.db, drive_user, drive_pass, {})
+
+        [xe_info] = self.models.execute_kw(self.db, self.uid, self.password, 'maintenance.equipment', 'read',
+                [body['equipment_id']],{'fields':['id', 'name', "owner_user_id", "last_request", "license_plate",
+                        "trip_count", "note", "message_ids"]})
+        body['name'] = f"Bảo trì xe {xe_info['license_plate']} - Ngày : {body['request_date']}"
         print('chot ha: ', body)
         for k in body.keys():
             if not body[k]:
