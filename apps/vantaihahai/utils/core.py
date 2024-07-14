b7f6f78db9f423b1af54a6868349f72d70baa7e8
DucAnh <ducanhvna@outlook.com>
2024-07-14 15:09:41 +0700

    Update core.py

--- apps/vantaihahai/utils/core.py
+++ apps/vantaihahai/utils/core.py
@@ -58,7 +58,22 @@ class VanTaiHaHai():
                         "location_dest_name", "location_id", "location_dest_id", 'eating_fee', 'note', 'odometer_start', 'odometer_dest',
                         'odometer_end', 'employee_id', 'schedule_date', 'start_date', 'end_date', 'attachment_ids', 'fleet_product_id',]})
         chat_id = 1
+        
         for item in fleets:
+            logs = self.models.execute_kw(self.db, self.uid, self.password, 'auditlog.log.line.view', 'search_read', [[('model_model','=','fleet.trip'),
+                ('res_id','=',item['id'])]],
+                {"fields":["id","old_value","new_value","create_date","model_model","res_id","user_id"],"limit":1000})
+            for log in logs:
+                dt = datetime.datetime.strptime(log["create_date"], "%Y-%m-%d")
+                msg = {
+                    'id': f'{unix_time_millis(dt)}',
+                    'content': f"{log['old_value']} -> {log['new_value']}" ,
+                    'thumbnail': '',
+                    'msg':f"{log['user_id']}: {log['old_value']} -> {log['new_value']}",
+                    'msgType': "TEXT",
+                    'senderId': 0,
+                    'reply': ''}
+                results.append(msg)
             item['fleet_product_id'] = {'id': item['fleet_product_id'][0], 'name': item['fleet_product_id'][1]} \
                     if item['fleet_product_id'] else None
             item['company_id'] ={'id':  item['company_id'][0] , 'name':item['company_id'][1]} \
