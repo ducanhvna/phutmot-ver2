eb0a4e844e51d6cb058939d022e97dfd89f84176
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-07-15 08:59:23 +0700

    update

--- apps/megavn/models.py
+++ apps/megavn/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class MegaEmployee(models.Model):
+    code = models.CharField(max_length=20,null=False, blank=False, unique=True)
+    group = models.CharField(max_length=50,null=True, blank=True)
+    name = models.CharField(max_length=200,null=False, blank=False)
+    department = models.CharField(max_length=50,null=True, blank=True)
+    other = models.CharField(max_length=50,null=True, blank=True)
+    title = models.CharField(max_length=50,null=True, blank=True)
+    email = models.CharField(max_length=100,null=True, blank=True)
+    
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
