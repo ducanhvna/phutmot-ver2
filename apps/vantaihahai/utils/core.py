fb5911749e80f1c02b81599f8ec24b484b0673c8
DucAnh <ducanhvna@outlook.com>
2024-07-21 13:46:23 +0700

    Update core.py

--- apps/vantaihahai/utils/core.py
+++ apps/vantaihahai/utils/core.py
@@ -216,9 +216,10 @@ class VanTaiHaHai():
                 item['location_name'] = item['location_name'] if item['location_name'] else None
                 item['schedule_date'] = item['schedule_date'] if item['schedule_date'] else None
                 item['location_dest_name'] = item['location_dest_name'] if item['location_dest_name'] else None
-                if len['attachment_ids'] >0:
-                    item['attachment_ids'] = self.models.execute_kw(self.db, self.uid, self.password, 'ir.attachment', 
-                        'read', item['attachment_ids'] ,{'fields': ['name', 'type', 'url', 'res_model', 'res_id']})
+                if item['attachment_ids']:
+                    if len(item['attachment_ids']) >0:
+                        item['attachment_ids'] = self.models.execute_kw(self.db, self.uid, self.password, 'ir.attachment', 
+                            'read', item['attachment_ids'] ,{'fields': ['name', 'type', 'url', 'res_model', 'res_id']})
 
         return {'data':{'results': results}, 'employee': {'id': employee_id}}
 
