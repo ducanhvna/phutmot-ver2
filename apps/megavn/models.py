1bbe1abe87b440d0f60fdc0c4810e6f2a84d8834
DucAnh <ducanhvna@outlook.com>
2024-07-25 06:30:58 +0700

    Update models.py

--- apps/megavn/models.py
+++ apps/megavn/models.py
@@ -8,7 +8,7 @@ class MegaEmployee(models.Model):
     other = models.CharField(max_length=50,null=True, blank=True)
     title = models.CharField(max_length=50,null=True, blank=True)
     email = models.CharField(max_length=100,null=True, blank=True)
-    
+    chat_id = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.code} - {self.name}'
