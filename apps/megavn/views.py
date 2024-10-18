2a387fb263d3e67d06f5861047d13ae9b9e313f5
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-10-18 11:32:59 +0700

    Update views.py

--- apps/megavn/views.py
+++ apps/megavn/views.py
@@ -83,15 +83,19 @@ class ErpLink(APIView):
         email = request.data.get('email')
         password = request.data.get('password')
         chat_id = request.data.get('chat_id')
-        deviceToken = request.data.get('deviceToken')
+        
         results = MegaEmployee.objects.filter(Q(code=email) )
         # | \
         #             (Q(email=email) &  Q(email__isnull=False)) | \
         #             (Q(other=email) &  Q(other__isnull=False)))
         for item in results:
             item.chat_id = chat_id
-            if (deviceToken):
-                item.device_token = deviceToken
+            try:
+                deviceToken = request.data.get('deviceToken')
+                if (deviceToken):
+                    item.device_token = deviceToken
+            except Exception as ex:
+                print(ex)
             if password:
                 item.password = password
             item.save()
