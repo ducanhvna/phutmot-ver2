c113a6e9e8987b52a5aaea3af7fe0ed2087d8092
DucAnh <ducanhvna@outlook.com>
2024-10-25 08:50:55 +0700

    Update views.py

--- apps/megavn/views.py
+++ apps/megavn/views.py
@@ -110,15 +110,16 @@ class ErpProfile(APIView):
                     if data_str:
                         data_dict = json.loads(data_str)
                         print(data_dict)
+                        result = data_dict
                 if(data_dict):
                 # print(response.text)
                     item = MegaEmployee(
                         code = code,
                         name = data_dict['FullName'],
                         department = data_dict['DepartmentName'],
-                        title = data_dict['JobTitleName'])
-                    if data_dict['WorkEmail'] and item:
-                        item.email = data_dict['WorkEmail']
+                        title = data_dict['JobTitleName'],
+                        # if data_dict['WorkEmail'] and item:
+                        email = data_dict['WorkEmail'])
                     item.save()
                     
                     result = {'EmployeeId': item.code, 
