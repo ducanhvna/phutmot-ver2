3c565481256c779f2d280b6167fdcd557c0734bd
DucAnh <ducanhvna@outlook.com>
2024-12-02 02:39:47 +0700

    Update urls.py

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
