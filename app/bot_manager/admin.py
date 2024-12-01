24a7d797912559fd497442bcc73868b50e1c6b59
DucAnh <ducanhvna@outlook.com>
2024-12-02 03:09:59 +0700

    Update admin.py

--- app/bot_manager/admin.py
+++ app/bot_manager/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Customer, Message
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'telegram_user_id')
+    search_fields = ('name', 'telegram_user_id')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'content', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('customer__name', 'content')
