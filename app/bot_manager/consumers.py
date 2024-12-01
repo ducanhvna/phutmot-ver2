3c5a5b0de2af44d4829f941f2c74cec6772807f6
DucAnh <ducanhvna@outlook.com>
2024-12-02 02:21:30 +0700

    Update consumers.py

--- app/bot_manager/consumers.py
+++ app/bot_manager/consumers.py
@@ -1,19 +1,19 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth.models import AnonymousUser
+# from django.contrib.auth.models import AnonymousUser
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        if isinstance(self.scope['user'], AnonymousUser):
-            await self.close()
-        else:
-            self.room_group_name = 'chat_%s' % self.scope['user'].id
-            await self.channel_layer.group_add(
-                self.room_group_name,
-                self.channel_name
-            )
-            await self.accept()
+        # if isinstance(self.scope['user'], AnonymousUser):
+        #     await self.close()
+        # else:
+        self.room_group_name = 'chat_%s' % self.scope['user'].id
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
