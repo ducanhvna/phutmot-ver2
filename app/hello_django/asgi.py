cf9ec6746e52c8ca58e9af60f98e6892e21faa8d
DucAnh <ducanhvna@outlook.com>
2024-12-02 02:19:25 +0700

    Update asgi.py

--- app/hello_django/asgi.py
+++ app/hello_django/asgi.py
@@ -1,4 +1,5 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
