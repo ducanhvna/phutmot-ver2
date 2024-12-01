746d32e6fc5fd139caedc41c6b484b844f88522f
DucAnh <ducanhvna@outlook.com>
2024-12-02 01:23:12 +0700

    update hinosoft.com

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -28,7 +28,7 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337", "http://localhost:8072", "https://hinosoft.com"]
 
 # Application definition
 
