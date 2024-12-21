844f40864d458fa07e67104e7069dab4561f0251
DucAnh <ducanhvna@outlook.com>
2024-12-21 20:02:39 +0700

    add template

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {'segment': 'index'}
+    html_template = loader.get_template('home/index.html')
+    return HttpResponse(html_template.render(context, request))
