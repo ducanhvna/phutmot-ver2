431d9a58ed4624bd5e59db1b1e0bf0c5d496a297
DucAnh <ducanhvna@outlook.com>
2024-12-21 20:44:04 +0700

    Update settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "app.context_processors.cfg_assets_root",
+                "context_processors.cfg_assets_root",
             ],
         },
     },
