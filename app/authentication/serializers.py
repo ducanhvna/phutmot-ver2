dc2df420d6f622f1b23e95a7e815d02b63654a55
DucAnh <ducanhvna@outlook.com>
2024-12-22 13:16:35 +0700

    Update serializers.py

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 class EmployeeSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-    job_id = serializers.CharField(max_length=255)
-    department_id = serializers.CharField(max_length=255)
+    job_id = serializers.JSONField()
+    department_id = serializers.JSONField()
     # code = serializers.CharField(max_length=255)
