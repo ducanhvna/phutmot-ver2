3cf59076451488d72121a4a3dc935c7415a1df6b
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-25 13:44:30 +0700

    Update admin.py

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Book
+
+
+@admin.register(Book)
+class Company(admin.ModelAdmin):
+    list_display = ('db', 'url', 'username', 'password', 'company_name')
+    search_fields = ('company_code', 'author')
