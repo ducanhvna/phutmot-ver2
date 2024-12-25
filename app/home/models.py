6a9ed79e185ef4ed746cac627de552f2bf54d87c
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-25 14:34:14 +0700

    Update models.py

--- app/home/models.py
+++ app/home/models.py
@@ -12,8 +12,6 @@ class Company(models.Model):
     info = JSONField(default=dict, blank=True)
     create_time = models.DateTimeField("Thời gian tạo", auto_now_add=True)
     update_time = models.DateTimeField("Thời gian cập nhật", auto_now=True)
-    created_user = models.CharField("Người tạo", max_length=255)
-    modified_user = models.CharField("Người sửa đổi", max_length=255)
 
     def __str__(self):
         return self.company_name
