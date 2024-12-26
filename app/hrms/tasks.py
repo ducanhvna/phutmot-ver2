8adce27e17789b2d052177469f84b229176755b7
DucAnh <ducanhvna@outlook.com>
2024-12-27 01:28:56 +0700

    Update tasks.py

--- app/hrms/tasks.py
+++ app/hrms/tasks.py
@@ -1,6 +1,9 @@
 # hrms/tasks.py
 
+import logging
 from celery import shared_task
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -8,13 +11,19 @@ def calculate_scheduling(attendance_id):
     # Import cục bộ để tránh import vòng lặp
     from .models import Attendance, Scheduling
 
-    # Lấy đối tượng Attendance
-    attendance = Attendance.objects.get(id=attendance_id)
+    try:
+        # Lấy đối tượng Attendance
+        attendance = Attendance.objects.get(id=attendance_id)
 
-    # Tính toán và cập nhật Scheduling tương ứng
-    # scheduling, created = Scheduling.objects.get_or_create(attendance=attendance)
+        # Log đối tượng Attendance
+        logger.info(f"Create attendance: {attendance}")
 
-    # Ví dụ: Cập nhật một số thuộc tính của Scheduling
-    # scheduling.some_field = attendance.some_related_field
-    # scheduling.save()
-    print("Create attendance", attendance)
+        # Tính toán và cập nhật Scheduling tương ứng
+        # scheduling, created = Scheduling.objects.get_or_create(attendance=attendance)
+
+        # Ví dụ: Cập nhật một số thuộc tính của Scheduling
+        # scheduling.some_field = attendance.some_related_field
+        # scheduling.save()
+
+    except Attendance.DoesNotExist:
+        logger.error(f"Attendance with id {attendance_id} does not exist.")
