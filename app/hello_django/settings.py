782b3b5b03db21b8fc6f65cecd4d9fd6fe982742
DucAnh <ducanhvna@outlook.com>
2024-12-27 00:43:43 +0700

    Update settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from celery import Celery
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
