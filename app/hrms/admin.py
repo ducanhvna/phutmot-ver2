2944a666afcaa7d7468fdb13655e65f55430b9f8
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-26 16:48:50 +0700

    Update admin.py

--- app/hrms/admin.py
+++ app/hrms/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Scheduling
+from .models import Scheduling, Leave
 
 
 @admin.register(Scheduling)
 class SchedulingAdmin(admin.ModelAdmin):
+    list_display = ('employee_code', 'start_date', 'end_date', 'create_time', 'update_time')
+    search_fields = ('employee_code', 'start_date', 'end_date')
+
+
+@admin.register(Leave)
+class LeaveAdmin(admin.ModelAdmin):
     list_display = ('employee_code', 'start_date', 'end_date', 'create_time', 'update_time')
     search_fields = ('employee_code', 'start_date', 'end_date')
