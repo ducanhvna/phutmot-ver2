2a3035dfb2b4d1f7f5f0a32646997324f81f172c
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-27 09:16:35 +0700

    Create upload_data_employee.py

---
+++ app/hrms/management/commands/upload_data_employee.py
@@ -0,0 +1,125 @@
+from django.core.management.base import BaseCommand
+from hrms.models import Employee
+import xmlrpc.client
+
+class Command(BaseCommand):
+    help = 'Upload Employee and Contract data from Django to Odoo'
+
+    def handle(self, *args, **kwargs):
+        # Define your Odoo connection parameters
+        url = 'https://hrm.mandalahotel.com.vn'
+        db = 'apechrm_product_v3'
+        username = 'admin_ho'
+        password = '43a824d3a724da2b59d059d909f13ba0c38fcb82'
+
+        common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
+        uid = common.authenticate(db, username, password, {})
+        models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+
+        # Get all Employee records from Django
+        employees = Employee.objects.all()
+
+        # Upload Employee data to Odoo
+        for employee in employees:
+            self.upload_employee_to_odoo(models, db, uid, password, employee)
+
+    def upload_employee_to_odoo(self, models, db, uid, password, employee):
+        # Check if employee already exists in Odoo
+        odoo_employee_id = models.execute_kw(
+            db, uid, password,
+            'hr.employee', 'search',
+            [[['code', '=', employee.employee_code]]]
+        )
+        
+        # Employee data to upload
+        employee_data = {
+            'name': employee.info.get('name'),
+            # 'user_id': employee.info.get('user_id'),
+            # 'employee_ho': employee.info.get('employee_ho'),
+            # 'part_time_company_id': employee.info.get('part_time_company_id'),
+            # 'part_time_department_id': employee.info.get('part_time_department_id'),
+            # 'company_id': employee.info.get('company_id'),
+            'company_id': 1,
+            'code': employee.employee_code,
+            # 'department_id': employee.info.get('department_id'),
+            # 'time_keeping_code': employee.info.get('time_keeping_code'),
+            # 'job_title': employee.info.get('job_title'),
+            # 'probationary_contract_termination_date': employee.info.get('probationary_contract_termination_date'),
+            'severance_day': employee.info.get('severance_day'),
+            'workingday': employee.info.get('workingday'),
+            # 'probationary_salary_rate': employee.info.get('probationary_salary_rate'),
+            # 'resource_calendar_id': employee.info.get('resource_calendar_id'),
+            # 'date_sign': employee.info.get('date_sign'),
+            # 'level': employee.info.get('level'),
+            # 'working_status': employee.info.get('working_status'),
+            # 'write_date': employee.info.get('write_date'),
+            'active': employee.info.get('active')
+        }
+
+        if odoo_employee_id:
+            # Update existing employee
+            models.execute_kw(
+                db, uid, password,
+                'hr.employee', 'write',
+                [odoo_employee_id, employee_data]
+            )
+            print(f"Updated employee {employee.employee_code} in Odoo")
+        else:
+            # Create new employee
+            models.execute_kw(
+                db, uid, password,
+                'hr.employee', 'create',
+                [employee_data]
+            )
+            print(f"Created employee {employee.employee_code} in Odoo")
+
+        # Upload related contracts
+        self.upload_contracts_to_odoo(models, db, uid, password, employee)
+
+    def upload_contracts_to_odoo(self, models, db, uid, password, employee):
+        contracts = employee.contracts  # Assuming contracts is a JSON field in Employee model
+        
+        for contract in contracts:
+            # Check if contract already exists in Odoo
+            odoo_contract_id = models.execute_kw(
+                db, uid, password,
+                'hr.contract', 'search',
+                [[['employee_code', '=', employee.employee_code], ['date_start', '=', contract.get('date_start')]]]
+            )
+            
+            # Contract data to upload
+            contract_data = {
+                'company_id': contract.get('company_id'),
+                # 'contract_type_id': contract.get('contract_type_id'),
+                # 'minutes_per_day': contract.get('minutes_per_day'),
+                'employee_code': employee.employee_code,
+                'employee_id': employee.info.get('id')[0] if employee.info.get('id') else employee.info.get('id'),
+                'date_end': contract.get('date_end'),
+                'date_start': contract.get('date_start'),
+                'date_sign': contract.get('date_sign'),
+                # 'salary_rate': contract.get('salary_rate'),
+                'state': contract.get('state'),
+                'active': contract.get('active'),
+                # 'start_end_attendance': contract.get('start_end_attendance'),
+                # 'resource_calendar_id': contract.get('resource_calendar_id'),
+                # 'depend_on_shift_time': contract.get('depend_on_shift_time'),
+                # 'by_hue_shift': contract.get('by_hue_shift'),
+                # 'write_date': contract.get('write_date')
+            }
+
+            if odoo_contract_id:
+                # Update existing contract
+                models.execute_kw(
+                    db, uid, password,
+                    'hr.contract', 'write',
+                    [odoo_contract_id, contract_data]
+                )
+                print(f"Updated contract for employee {employee.employee_code} in Odoo")
+            else:
+                # Create new contract
+                models.execute_kw(
+                    db, uid, password,
+                    'hr.contract', 'create',
+                    [contract_data]
+                )
+                print(f"Created contract for employee {employee.employee_code} in Odoo")
