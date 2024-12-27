9985f5a7b4e6eb8bdc5f5d55e6894047f0fc6698
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-27 09:04:54 +0700

    Update admin.py

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, UserProfile
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('db', 'url', 'username', 'password', 'company_name', 'company_code')
     search_fields = ('company_code', 'company_code')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('employee_code', 'create_time', 'update_time', 'user')
+    search_fields = ('employee_code', 'user')
