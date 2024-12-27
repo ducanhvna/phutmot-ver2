f0e80e1f5f1274731c6aa6f1d0bad3da85e081ed
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-12-27 13:44:11 +0700

    Update admin.py

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Company, UserProfile
+from django.utils.html import format_html
+import json
 
 
 @admin.register(Company)
@@ -10,5 +12,21 @@ class CompanyAdmin(admin.ModelAdmin):
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('employee_code', 'create_time', 'update_time', 'user')
+    list_display = ('employee_code', 'display_contracts', 'create_time', 'update_time', 'user')
     search_fields = ('employee_code', 'user')
+
+    def display_contracts(self, obj):
+        try:
+            contracts = obj.contracts
+            html = '<table>'
+            for contract in contracts:
+                html += '<tr>'
+                for key, value in contract.items():
+                    html += f'<th>{key}</th><td>{value}</td>'
+                html += '</tr>'
+            html += '</table>'
+            return format_html(html)
+        except json.JSONDecodeError:
+            return "Invalid JSON"
+
+    display_contracts.short_description = 'Danh sách hợp đồng'
