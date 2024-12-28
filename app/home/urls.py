8754d199b102d611e95d8a035f2900a401db23e0
DucAnh <ducanhvna@outlook.com>
2024-12-29 01:39:15 +0700

    Update urls.py

--- app/home/urls.py
+++ app/home/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     re_path('timesheet/', views.timesheet, name='timesheet'),
-    re_path('api/get_details/', views.get_details, name='detail_timesheet'),
+    re_path('api/get_details', views.get_details, name='detail_timesheet'),
     re_path(r'^.*\.*', views.pages, name='pages'),
 ]
