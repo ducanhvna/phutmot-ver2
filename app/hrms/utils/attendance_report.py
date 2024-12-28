9158d2c9df50d99b7cb5882584b5444945bece44
DucAnh <ducanhvna@outlook.com>
2024-12-28 20:47:23 +0700

    Update attendance_report.py

--- app/hrms/utils/attendance_report.py
+++ app/hrms/utils/attendance_report.py
@@ -15,7 +15,6 @@ class InoutMode:
 
 def calculate_worktime_without_inout(realTimein, realTimeout, scheduling_record):
     result = 0
-    totalWorkTime = ['totalWorkTime']
     restStartDateTime = scheduling_record['restStartDateTime']
     restEndDateTime = scheduling_record['restEndDateTime']
     shiftStartDateTime = scheduling_record['shiftStartDateTime']
@@ -42,12 +41,13 @@ def calculate_Holiday_worktime_without_inout(realTimein, realTimeout, scheduling
     holidayEndDatetime = scheduling_record['holidayEndDatetime']
     restStartDateTime = scheduling_record['restStartDateTime']
     restEndDateTime = scheduling_record['restEndDateTime']
-    shiftStartDateTime = scheduling_record['shiftStartDateTime']
-    shiftEndDateTime = scheduling_record['shiftEndDateTime']
+    # shiftStartDateTime = scheduling_record['shiftStartDateTime']
+    # shiftEndDateTime = scheduling_record['shiftEndDateTime']
 
     result = 0
     if (shift is not None and isHoliday and 
-        (calculate_worktime_without_inout(holidayStartDatetime, holidayEndDatetime) > 0 or 'PH' in shiftName)):
+         (calculate_worktime_without_inout(holidayStartDatetime, holidayEndDatetime) > 0
+         or 'PH' in shiftName)):
 
         stageStart = restStartDateTime if restStartDateTime < holidayEndDatetime else holidayEndDatetime
         current_program = (realTimeout.replace(second=0) if realTimeout < stageStart else stageStart) - (realTimein.replace(second=0) if realTimein > holidayStartDatetime else holidayStartDatetime)
@@ -60,7 +60,7 @@ def calculate_Holiday_worktime_without_inout(realTimein, realTimeout, scheduling
         result = stage_fist + stage_second
     return result
 
-def calculate_Night_Holiday_without_inout(realTimein, realTimeout):
+def calculate_Night_Holiday_without_inout(realTimein, realTimeout, scheduling_record, shift):
     holidayNightStageFistStartDatetime = scheduling_record['holidayNightStageFistStartDatetime']
     holidayNightStageFistEndDatetime = scheduling_record['holidayNightStageFistEndDatetime']
     holidayNightStageLastStartDatetime = scheduling_record['holidayNightStageLastStartDatetime']
