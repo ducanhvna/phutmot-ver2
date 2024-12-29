b322d592202a40db1231bf6fee0c02e97f1bd813
DucAnh <ducanhvna@outlook.com>
2024-12-29 22:27:33 +0700

    Update attendance_report.py

--- app/hrms/utils/attendance_report.py
+++ app/hrms/utils/attendance_report.py
@@ -958,7 +958,14 @@ def process_worktime_ho(scheduling_record, date, shift_start_datetime, shift_end
 
     for leave_item in [element for element in list_workingout_leaves if element.for_reasons == '1' and element.attendance_missing_from is not None and element.attendance_missing_to is not None]:
         process_leave_item_ho(scheduling_record, leave_item)
-
+def is_business_leave(leave, shift_end_datetime, shift_start_datetime):
+    return (
+        leave.get('request_date_from') is not None
+        and leave.get('request_date_to') is not None
+        and not leave['request_date_from'].replace(hour=0, minute=0, second=0) > shift_end_datetime
+        and not leave['request_date_to'].replace(hour=23, minute=59, second=59) < shift_start_datetime
+        and 'công tác' in leave['holiday_status_name'].lower()
+    )
 
 def process_business_leave(hr_leaves, date, shift_end_datetime, shift_start_datetime, total_shift_work_time_calculate, shift_name, minutes_per_day, late_in_time):
     time_business_trip = 0
@@ -966,16 +973,10 @@ def process_business_leave(hr_leaves, date, shift_end_datetime, shift_start_date
     if date is None or shift_end_datetime is None or shift_start_datetime is None:
         list_business_leaves = []
     else:
-        time_business_trip = min(
-            total_shift_work_time_calculate == 0
-            and shift_name not in ['OFF', 'UP', '-']
-            and shift_name is not None
-            and len(shift_name) > 1
-            and '/' not in shift_name
-            and minutes_per_day
-            or total_shift_work_time_calculate,
-            time_business_trip + max(leave_item['minutes'], leave_item['time_minute'])
-        )
+        list_business_leaves = list(filter(
+            lambda leave: is_business_leave(leave, shift_end_datetime, shift_start_datetime),
+            hr_leaves
+        ))
 
 
     for leave_item in list_business_leaves:
