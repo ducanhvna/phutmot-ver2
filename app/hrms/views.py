4fabc1cf67319e8768b373b6fb4db7d85f9764f2
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-01-02 09:59:01 +0700

    Update views.py

--- app/hrms/views.py
+++ app/hrms/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def taskcreate(request):
     context = {}
     html_template = loader.get_template('hrms/task_create.html')
