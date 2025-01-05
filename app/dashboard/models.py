9a85d399c141dfa94d6085222c2f21c8a88df593
DucAnh <ducanhvna@outlook.com>
2025-01-05 18:46:49 +0700

    add fleet to setting

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -10,3 +10,14 @@ class Hrms(models.Model):
 
     def __str__(self):
         return self.company_name
+
+
+class Fleet(models.Model):
+    company_code = models.CharField(max_length=50, unique=True)
+    company_name = models.CharField(max_length=255)
+    info = models.JSONField(default=dict, blank=True)
+    create_time = models.DateTimeField("Thời gian tạo", auto_now_add=True)
+    update_time = models.DateTimeField("Thời gian cập nhật", auto_now=True)
+
+    def __str__(self):
+        return self.company_name
