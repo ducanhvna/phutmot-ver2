2a2ee72fca4303f233e18184a33f08e3a554495a
DucAnh <ducanhvna@outlook.com>
2025-01-05 16:32:22 +0700

    Update models.py

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Hrms(models.Model):
+    company_code = models.CharField(max_length=50, unique=True)
+    company_name = models.CharField(max_length=255)
+    info = models.JSONField(default=dict, blank=True)
+    create_time = models.DateTimeField("Thời gian tạo", auto_now_add=True)
+    update_time = models.DateTimeField("Thời gian cập nhật", auto_now=True)
+
+    def __str__(self):
+        return self.company_name
