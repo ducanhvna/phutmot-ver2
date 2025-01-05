798b0fe5f478b4fea53bbc30b64fe723191d451b
DucAnh <ducanhvna@outlook.com>
2025-01-05 16:35:50 +0700

    Update tasks.py

--- app/dashboard/tasks.py
+++ app/dashboard/tasks.py
@@ -9,6 +9,6 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def check_apec_hrms_update():
-    logger.info(f"Create attendance check apecinpu")
+    logger.info("Create attendance check apecinput")
     print("Tác vụ chạy định kỳ mỗi 30 giây check_apec_hrms_update")
     # Thực hiện công việc của bạn tại đây
