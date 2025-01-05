d5dfb35f744914dcb716d34f17b11b4f68e89032
DucAnh <ducanhvna@outlook.com>
2025-01-05 18:57:25 +0700

    Update models.py

--- app/fleet/models.py
+++ app/fleet/models.py
@@ -6,10 +6,9 @@ class Vehicle(models.Model):
     start_date = models.DateField("Ngày bắt đầu tháng")
     end_date = models.DateField("Ngày kết thúc tháng")
     info = models.JSONField("Thông tin xe", default=dict, blank=True)
-    other_profile = JSONField("Thông tin khác", default=dict, blank=True)
+    other_profile = models.JSONField("Thông tin khác", default=dict, blank=True)
     create_time = models.DateTimeField("Thời gian tạo", auto_now_add=True)
     update_time = models.DateTimeField("Thời gian cập nhật", auto_now=True)
- 
 
     class Meta:
         constraints = [
