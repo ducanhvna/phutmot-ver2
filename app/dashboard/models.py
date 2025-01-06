79f4935170f03b1c021674a711514a9f36705de1
DucAnh <ducanhvna@outlook.com>
2025-01-06 20:21:53 +0700

    Update models.py

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -23,3 +23,16 @@ class Fleet(models.Model):
 
     def __str__(self):
         return self.company_name
+
+
+class Agriculture(models.Model):
+    company_code = models.CharField(max_length=50, unique=True)
+    company_name = models.CharField(max_length=255)
+    start_date = models.DateField("Ngày bắt đầu tháng")
+    end_date = models.DateField("Ngày kết thúc tháng")
+    info = models.JSONField(default=dict, blank=True)
+    create_time = models.DateTimeField("Thời gian tạo", auto_now_add=True)
+    update_time = models.DateTimeField("Thời gian cập nhật", auto_now=True)
+
+    def __str__(self):
+        return self.company_name
