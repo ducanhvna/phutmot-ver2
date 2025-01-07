4b92f393805ef56f3340cdc0ff4b8fdc400d6441
DucAnh <ducanhvna@outlook.com>
2025-01-07 23:27:49 +0700

    update src

---
+++ app/dashboard/management/commands/download_and_import_leave.py
@@ -0,0 +1,18 @@
+# your_app/management/commands/download_and_import_data.py
+
+from django.core.management.base import BaseCommand
+from dashboard.utils.hrms import HrmsDashboard
+from datetime import datetime
+
+
+class Command(BaseCommand):
+    help = "Download Leave Trip data from Odoo and import into Django"
+
+    def handle(self, *args, **kwargs):
+        # Get the first day of the current month
+        hrms_dashboard = HrmsDashboard()
+        hrms_dashboard.update()
+        first_day_of_month = datetime.now().replace(day=1)
+        last_day_of_last_month = first_day_of_month - timedelta(days=1)
+        first_day_of_last_month = last_day_of_last_month.replace(day=1)
+        hrms_dashboard.update(first_day_of_last_month)
---
+++ app/dashboard/utils/hrms.py
@@ -0,0 +1,60 @@
+from hrms.utils.hrleave import LeaveService
+# from collections import defaultdict
+from hrms.models import Leave
+import xmlrpc.client
+from dashboard.models import Hrms
+from datetime import datetime, timedelta
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+
+class HrmsDashboard():
+    def update(self, first_day_of_month=None):
+        # Get the first day of the current month
+        max_write_date_leave = None
+        if not first_day_of_month:
+            first_day_of_month = datetime.now().replace(day=1)
+        leave = LeaveService(first_day_of_month)
+        hrms_dashboard, created = Hrms.objects.get_or_create(
+            company_code="APEC",
+            start_date=first_day_of_month,
+            end_date=leave.last_day_of_month,
+            defaults={"info": {}},
+        )
+        info = hrms_dashboard.info
+        if info and (info != {}):
+            max_write_date_leave = info["max_write_date_leave"]
+        new_write_date = leave.download(max_write_date_leave)
+        hrms_dashboard.info["max_write_date_leave"] = (
+            new_write_date.strftime("%Y-%m-%d %H:%M:%S") if new_write_date else None
+        )
+        today_leaves, latest_leaves = self.get_today_task()
+        hrms_dashboard.info['today_leaves'] = today_leaves
+        hrms_dashboard.info['latest_leaves'] = latest_leaves
+        hrms_dashboard.save()
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "broadcast",
+            {
+                "type": "chat_message",
+                "message": "This is a broadcast message",
+                "latest_leaves": [],
+            },
+        )
+
+    def get_today_task(self):
+        first_day_of_month = datetime.now().replace(day=1)
+        last_day_of_last_month = first_day_of_month - timedelta(days=1)
+        list_leaves = Leave.objects.filter(
+            start_date=last_day_of_last_month.date()
+        )
+        today_leave = []
+        latest_leave = []
+        for leave in list_leaves:
+            # print(vehicle)
+            leave_records = leave.leave_records
+            # today_leave.extend(leaves)
+            max_leave = max(leave_records, key=lambda x: x['id']) if leave_records else None
+        if max_leave:
+            latest_leave.append(max_leave)
+        return today_leave, latest_leave
