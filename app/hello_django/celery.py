dc3f4f13ecab4817c9c1c092ca2e79cbd00b2712
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-01-10 10:38:56 +0700

    Update celery.py

--- app/hello_django/celery.py
+++ app/hello_django/celery.py
@@ -16,6 +16,6 @@ app.conf.beat_schedule = {
     },
     'hrm-scheduled-task': {  # Task mới
         'task': 'dashboard.tasks.update_apec_hrm',
-        'schedule': 240.0,  # 2 phút một lần
+        'schedule': 480.0,  # 2 phút một lần
     },
 }
