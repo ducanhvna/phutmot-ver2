c554e73baf8e6ccdb70ad845aee1e4d3e72b202b
DucAnh <ducanhvna@outlook.com>
2025-01-11 17:26:54 +0700

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -24,13 +24,12 @@ class EmployeeSearchAPIView(generics.ListAPIView):
         query = self.request.query_params.get('q')
         if query:
             query = unidecode(query).lower()
-            search_conditions = Q(employee_code__icontains=query) | \
+            queryset = queryset.filter(employee_code__icontains=query) | \
                 Q(start_date__icontains=query) | \
                 Q(time_keeping_code__icontains=query) | \
                 Q(info_unaccented__name__icontains=query) | \
                 Q(info_unaccented__code__icontains=query) | \
                 Q(info_unaccented__time_keeping_code__icontains=query)
-            queryset = queryset.filter(search_conditions)
         return queryset
 
 
