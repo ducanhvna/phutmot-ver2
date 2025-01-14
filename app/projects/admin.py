2e7ce0e71d09410c62173246fcd6c28b857d341c
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-01-14 12:18:14 +0700

    Update admin.py

--- app/projects/admin.py
+++ app/projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('code', 'company_code', 'name', 'company_name', 'start_date', 'end_date')
+    search_fields = ('code', 'company_code', 'name', 'company_name', 'start_date', 'end_date')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('employee_code', 'start_date', 'end_date')
+    search_fields = ('employee_code', 'start_date', 'end_date')
