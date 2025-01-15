7eaa3dd0c49d0c28f7daaeb08ddb7bb109cfd4a8
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-01-15 13:09:02 +0700

    Update settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -185,3 +185,5 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
