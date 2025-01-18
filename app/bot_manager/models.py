a79d9771263aaf41c6b923683c99478b3fec2e21
DucAnh <ducanhvna@outlook.com>
2025-01-18 15:54:03 +0700

    Revert "Update models.py"
    This reverts commit 0cf2b3fa8946594eb5b20e8e89b184f29e1d15a4.

--- app/bot_manager/models.py
+++ app/bot_manager/models.py
@@ -208,7 +208,7 @@ class Page(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Chatuser(models.Model):
+class User(models.Model):
     id = models.BigAutoField(primary_key=True)
     identity = models.CharField(max_length=255)
     username = models.CharField(max_length=255, null=True, blank=True)
