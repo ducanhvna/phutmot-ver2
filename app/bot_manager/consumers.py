a6fd18fde548c548f7cef219ad10ffb8636358ab
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-01-21 16:37:36 +0700

    Update consumers.py

--- app/bot_manager/consumers.py
+++ app/bot_manager/consumers.py
@@ -28,8 +28,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
         print("receive: ", text_data_json)
+        message = text_data_json['message']
         await self.channel_layer.group_send(
             self.room_group_name,
             {
