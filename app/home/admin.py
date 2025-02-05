f7cfc0e95de839dabe9f6e9c0bde7ac66ec804da
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-02-05 08:07:33 +0700

    Update admin.py

--- app/home/admin.py
+++ app/home/admin.py
@@ -13,7 +13,7 @@ class CompanyAdmin(admin.ModelAdmin):
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('employee_code', 'create_time', 'update_time', 'user')
-    search_fields = ('employee_code')
+    search_fields = ('employee_code', )
 
     def display_contracts(self, obj):
         try:
