856d19be43bd69f035223df1208f39bb4fc92388
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-03-31 13:33:25 +0700

    Update views.py

--- app/bot_manager/views.py
+++ app/bot_manager/views.py
@@ -610,9 +610,10 @@ class FetchRandomRoomsView(APIView):
 
         else:
             new_room_data = {
+                'id': 9000,
                 'admin_id': 0,
                 'photo': 'default_photo_url',  # Replace this with a real photo URL
-                'title': 'Sức Khỏe',
+                'title': 'Sức Khỏe (Health) ',
                 'desc': 'New room added for authenticated user',
                 'interest_ids': '1,2,3',  # Replace with real interest data
                 'is_private': 0,
@@ -629,6 +630,50 @@ class FetchRandomRoomsView(APIView):
             new_room_data_serialized['private_user_id'] = user_id  # Add private_user_id directly in serialized data
             # rooms.insert(0, new_room_instance)  # Add the new room to the beginning of the list
             rooms.insert(0, new_room_data_serialized)  # Add the new room to the beginning of the list
+
+            new_room_data = {
+                'id': 9001,
+                'admin_id': 0,
+                'photo': 'default_photo_url',  # Replace this with a real photo URL
+                'title': 'Học Tập (Education)',
+                'desc': 'New room added for authenticated user',
+                'interest_ids': '1,2,3',  # Replace with real interest data
+                'is_private': 0,
+                'is_join_request_enable': 1,
+                'total_member': 1,
+                'created_at': timezone.now(),
+                'updated_at': timezone.now()
+            }
+
+            new_room_instance = Room(**new_room_data)
+            new_room_instance.user_room_status = 5
+            new_room_serializer = RoomSerializer(new_room_instance)
+            new_room_data_serialized = new_room_serializer.data
+            new_room_data_serialized['private_user_id'] = user_id  # Add private_user_id directly in serialized data
+            # rooms.insert(0, new_room_instance)  # Add the new room to the beginning of the list
+            rooms.insert(1, new_room_data_serialized)  # Add the new room to the beginning of the list
+
+            new_room_data = {
+                'id': 9002,
+                'admin_id': 0,
+                'photo': 'default_photo_url',  # Replace this with a real photo URL
+                'title': 'Kỷ niệm (Education)',
+                'desc': 'New room added for authenticated user',
+                'interest_ids': '1,2,3',  # Replace with real interest data
+                'is_private': 0,
+                'is_join_request_enable': 1,
+                'total_member': 1,
+                'created_at': timezone.now(),
+                'updated_at': timezone.now()
+            }
+
+            new_room_instance = Room(**new_room_data)
+            new_room_instance.user_room_status = 5
+            new_room_serializer = RoomSerializer(new_room_instance)
+            new_room_data_serialized = new_room_serializer.data
+            new_room_data_serialized['private_user_id'] = user_id  # Add private_user_id directly in serialized data
+            # rooms.insert(0, new_room_instance)  # Add the new room to the beginning of the list
+            rooms.insert(2, new_room_data_serialized)  # Add the new room to the beginning of the list
         room_serializer = RoomSerializer(rooms, many=True)
 
         response_data = {
