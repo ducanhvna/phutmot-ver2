303725956508e71412e20afcd535d6ca8271059f
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-04-10 16:18:44 +0700

    Update urls.py

--- app/devices/urls.py
+++ app/devices/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('fetchProductPods/', ProductCategoryAPIView.as_view(), name='fetch_pod_products'),
     path('fetchProductPrice/', PriceAPIView.as_view(), name='fetch_price_products'),
     path('fetchRoomDetail/', fetchRoomDetailView.as_view(), name='fetch_room_detail'),
-    path('fetchAiDataSupport/', AiDataSupportView.as_view(), name='fetch_ai_data_support')
+    path('fetchAiDataSupport/', AiDataSupportView.as_view(), name='fetch_ai_data_support'),
+    path('fetchMyOwnRooms/', FetchRoomChatsView.as_view(), name='fetch_my_own_room_chats')
 ]
