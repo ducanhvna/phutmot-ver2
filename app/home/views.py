c7c08b651c5b93c76bb5f32dbf20e36580823f57
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-04-22 13:42:53 +0700

    Update src

--- app/home/views.py
+++ app/home/views.py
@@ -470,6 +470,9 @@ def split_data_by_week(data, month, year):
     for entry in data:
         try:
             entry_date = datetime.strptime(entry["date"], "%Y-%m-%d")
+            entry['month'] = entry_date.month
+            entry['year'] = entry_date.year
+            entry['day'] = entry_date.day
             if (entry_date.month == month) and (entry_date.year == year):
                 week_number = entry_date.isocalendar()[1]  # Lấy số tuần trong năm
             if not (f"{week_number}" in weeks):
