ac5281853b14e8a4f725ae05c0d55fa5adb9813a
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-04-22 11:12:10 +0700

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -466,13 +466,13 @@ class LoginView(APIView):
 
 
 def split_data_by_week(data, month, year):
-    weeks = defaultdict(list)
+    weeks = defaultdict(lambda: defaultdict(list))
     for entry in data:
         try:
             entry_date = datetime.strptime(entry["date"], "%Y-%m-%d")
             if (entry_date.month == month) and (entry_date.year == year):
                 week_number = entry_date.isocalendar()[1]  # Lấy số tuần trong năm
-                weeks[f"{week_number}"].append(entry)
+                weeks[f"w{week_number}"].append(entry)
         except ValueError:
             print(f"Lỗi: Định dạng ngày tháng không hợp lệ ({entry['date']})")
 
