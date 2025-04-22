4a36964a4d7df7b53e49e02bf10fe4303b35cc62
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-04-22 09:33:19 +0700

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -484,7 +484,7 @@ def personal_timesheet(request):
     # last_month = datetime(year=year, month=month, day=1) - timedelta(days=1)
     # attendance = Attendance.objects.get(code=f'{2630}', start_date__month=last_month.month, start_date__year=last_month.year)
     # start_date = attendance.start_date + timedelta(days=1)
-    employees = Employee.objects.filter(employee_code=code)
+    employees = Employee.objects.all()
     # scheduling = Scheduling.objects.get(employee_code='code', start_date=start_date)
     html_template = loader.get_template("home/tables.html")
     data = [
@@ -493,7 +493,7 @@ def personal_timesheet(request):
             "code": emplyee['code'],
             "timesheet": [
                 {
-                    "month": 3,
+                    "month": emplyee.start_date.month,
                     "week": 1,
                     "data": [
                         {"date": 0, "shift_name": "8EG"},
