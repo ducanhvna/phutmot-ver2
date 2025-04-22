82393d3c8c364755baff7c5d4490e2934fe4a3dc
anhndapec <129018473+anhndapec@users.noreply.github.com>
2025-04-22 12:04:56 +0700

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -472,6 +472,9 @@ def split_data_by_week(data, month, year):
             entry_date = datetime.strptime(entry["date"], "%Y-%m-%d")
             if (entry_date.month == month) and (entry_date.year == year):
                 week_number = entry_date.isocalendar()[1]  # Lấy số tuần trong năm
+            if not weeks[f"w{week_number}"]:
+                weeks[f"w{week_number}"]=[entry]
+            else:
                 weeks[f"w{week_number}"].append(entry)
         except ValueError:
             print(f"Lỗi: Định dạng ngày tháng không hợp lệ ({entry['date']})")
