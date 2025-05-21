d6e44a7d2aa35de49dd7894f43d65de8c4a60d22
DI-anhnd <nguyen.duc.anh@dataimpact.vn>
2025-05-21 15:34:52 +0700

    add django-cors-headers

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     # Third-party apps
     "channels",
     "upload",
@@ -98,6 +99,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -218,5 +220,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://hinosoft.com"
 ]
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
+
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
