f68f054210dd31666c52b28e59cfefd0454a3a8f
DI-anhnd <nguyen.duc.anh@dataimpact.vn>
2025-05-21 16:09:44 +0700

    update

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -221,9 +221,8 @@ ERP_PASSWORD = "admin"
 #     "https://hinosoft.com"
 # ]
 # CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-# CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
-
+CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-CORS_ALLOW_HEADERS = ["*"]
+# CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
