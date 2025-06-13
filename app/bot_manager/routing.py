83712d0576c94b90158a3aee9b18c83eda17b9bf
DI-anhnd <nguyen.duc.anh@dataimpact.vn>
2025-06-13 15:30:52 +0700

    delete redundant file

--- app/bot_manager/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from notifications.consumers import NotificationConsumer
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/chat/', consumers.ChatConsumer.as_asgi()),
-    path('ws/notifications/', NotificationConsumer.as_asgi()),
-]
--- app/documents/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import upload_document, document_detail, oauth2callback, authorize
-
-urlpatterns = [
-    path('', oauth2callback, name='oauth2callback'),
-    path('/upload/', upload_document, name='upload_document'),
-    path('/<int:id>/', document_detail, name='document_detail'),
-    path('/authorize/', authorize, name='authorize'),
-]
