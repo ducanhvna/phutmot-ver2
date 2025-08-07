7a0764fb6f70328c11cf79d05ed88a3cdac723b8
DucAnh <ducanhvna@outlook.com>
2025-08-07 15:26:22 +0700

    Update utils.py

--- backend/apps/products/utils.py
+++ backend/apps/products/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import xmlrpc.client
 
 def get_all_products(url, db, username, password, limit=10):
@@ -25,6 +26,54 @@ def get_all_products(url, db, username, password, limit=10):
         offset += limit
     return all_products
 
+
+def get_product_detail(url, db, username, password, product_id):
+    common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
+    uid = common.authenticate(db, username, password, {})
+    models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
+    # Lấy chi tiết product.template
+    product = models.execute_kw(
+        db, uid, password,
+        'product.template', 'read',
+        [[product_id]],
+        {'fields': ['id', 'name', 'attribute_line_ids']}
+    )
+    if not product:
+        return None
+    product = product[0]
+    # Lấy chi tiết các attribute_line_ids
+    attribute_lines = []
+    if product.get('attribute_line_ids'):
+        attribute_lines = models.execute_kw(
+            db, uid, password,
+            'product.template.attribute.line', 'read',
+            [product['attribute_line_ids']],
+            {'fields': ['id', 'attribute_id', 'value_ids']}
+        )
+        # Lấy thông tin value cho từng attribute line
+        for line in attribute_lines:
+            # Lấy tên attribute
+            attribute = models.execute_kw(
+                db, uid, password,
+                'product.attribute', 'read',
+                [ [line['attribute_id'][0]] ] if line['attribute_id'] else [],
+                {'fields': ['name']}
+            )
+            line['attribute_name'] = attribute[0]['name'] if attribute else None
+            # Lấy danh sách value
+            values = []
+            if line.get('value_ids'):
+                values = models.execute_kw(
+                    db, uid, password,
+                    'product.attribute.value', 'read',
+                    [line['value_ids']],
+                    {'fields': ['name']}
+                )
+            line['values'] = [v['name'] for v in values]
+    product['attribute_lines'] = attribute_lines
+    return product
+
+
 # Ví dụ gọi hàm
 if __name__ == "__main__":
     url = "http://solienlacdientu.info"
@@ -33,3 +82,7 @@ if __name__ == "__main__":
     password = "admin"
     products = get_all_products(url, db, username, password, limit=10)
     print(products)
+
+    product_id = 83  # Thay bằng id thực tế
+    detail = get_product_detail(url, db, username, password, product_id)
+    print(detail)
