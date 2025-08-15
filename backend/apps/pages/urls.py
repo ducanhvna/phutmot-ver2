b5fe37caddb0e6bd34a2eb7552fe7c9d08674fc8
DucAnh <ducanhvna@outlook.com>
2025-08-15 11:15:58 +0700

    add pos

--- backend/apps/pages/urls.py
+++ backend/apps/pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pos', views.pos, name='pos'),
+    path('pos/list-product', views.pos_list_product, name='pos_list_product'),
 ]
--- backend/apps/pages/views.py
+++ backend/apps/pages/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    print("Request received at index view")  # Debugging line
     # Page from the theme 
     return render(request, 'pages/index.html', {'segment': 'dashboard'})
 
 
 def pos(request):
-    print("Request received at pos view")  # Debugging line
 
     # Page from the theme 
     return render(request, 'backend/index.html', {'segment': 'pos'})
+
+def pos_list_product(request):
+    print("Request received at pos_list_product view")  # Debugging line
+
+    # Page from the theme 
+    return render(request, 'backend/pos-list-product.html', {'segment': 'pos_list_product'})
