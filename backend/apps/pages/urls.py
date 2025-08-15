6cbd563483d1bd11220f5a6b73ee70b5df6eff25
DucAnh <ducanhvna@outlook.com>
2025-08-15 10:12:18 +0700

    update static

--- backend/apps/pages/urls.py
+++ backend/apps/pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/pos', views.pos, name='post'),
+    path('pos', views.pos, name='pos'),
 ]
--- backend/apps/pages/views.py
+++ backend/apps/pages/views.py
@@ -4,12 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-
+    print("Request received at index view")  # Debugging line
     # Page from the theme 
     return render(request, 'pages/index.html', {'segment': 'dashboard'})
 
 
 def pos(request):
+    print("Request received at pos view")  # Debugging line
 
     # Page from the theme 
     return render(request, 'backend/index.html', {'segment': 'pos'})
