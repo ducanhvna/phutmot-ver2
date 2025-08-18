f4cc206ea1e97be293d6fc22a904583e0d5d905a
Dương Công Thái <132037088+duongthai187@users.noreply.github.com>
2025-08-18 17:40:10 +0700

    Add files via upload

---
+++ backend/addons/btmh_integration/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Import models mới với tên tiếng Việt
+from . import btmh_doanh_thu_ban_hang
+from . import btmh_chung_tu_thanh_toan
+from . import btmh_dat_coc
+from . import btmh_du_dau_ngay
+from . import btmh_nhat_ky_dong_bo
+from . import res_partner
+from . import account_move
