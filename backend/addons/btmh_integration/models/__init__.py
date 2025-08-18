e88fdb7dc63dce17922bb4475471c999ec14da77
Dương Công Thái <132037088+duongthai187@users.noreply.github.com>
2025-08-18 17:42:34 +0700

    Update __init__.py

--- backend/addons/btmh_integration/models/__init__.py
+++ backend/addons/btmh_integration/models/__init__.py
@@ -5,6 +5,3 @@ from . import btmh_doanh_thu_ban_hang
 from . import btmh_chung_tu_thanh_toan
 from . import btmh_dat_coc
 from . import btmh_du_dau_ngay
-from . import btmh_nhat_ky_dong_bo
-from . import res_partner
-from . import account_move
