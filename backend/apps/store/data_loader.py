6a310437458d3b5fe294bd2a8f5a34c47725e1c0
btmh-anhnd <nguyenducanh.it@baotinmanhhai.vn>
2025-12-10 03:59:41 +0700

    update rates

--- backend/apps/store/data_loader.py
+++ backend/apps/store/data_loader.py
@@ -1,9 +1,9 @@
 # file: data_loader.py
 import pandas as pd
 
-df_dmH = pd.read_parquet('data/Product/DmH.parquet')
-df_dmQTTG = pd.read_parquet('data/Product/DmQTTG.parquet')
-df_dmVBTG = pd.read_parquet('data/Product/DmVbMM.parquet')
+# df_dmH = pd.read_parquet('data/Product/DmH.parquet')
+# df_dmQTTG = pd.read_parquet('data/Product/DmQTTG.parquet')
+# df_dmVBTG = pd.read_parquet('data/Product/DmVbMM.parquet')
 
 # Nếu bạn cần gửi payload (JSON, form...), ví dụ:
 CLIENT_CERT_PATH = "data/Product/client_cert.pem"
--- backend/apps/store/views.py
+++ backend/apps/store/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
 from .authentication import JWTAuthentication
 import pandas as pd
-from .data_loader import df_dmH, df_dmQTTG, df_dmVBTG, cert, CA_CERT_PATH
+from .data_loader import cert, CA_CERT_PATH
 import math
 import numpy as np
 import datetime
@@ -393,8 +393,8 @@ url = "https://14.224.192.52:9999/api/v1/calculate-price"
 #     verify= CA_CERT_PATH # hoặc verify=False nếu chỉ test
 # )
 
-def tinh_gia_ban(row):
-    sku = f"{row['Ma_Hang']}"
+def tinh_gia_ban(sku):
+    # sku = f"{row['Ma_Hang']}"
     response = requests.post(
         url,
         json={"ma_hang": sku},
@@ -407,96 +407,94 @@ def tinh_gia_ban(row):
     # vbtg_row = df_dmVBTG[df_dmVBTG['ID'] == row['ID_VBTG']].squeeze()
 
     # Các biến cần thiết
-    TL_KLoai = row['T_Luong']
-    TL_Da_C = row['The_Tich']
-    T_LuongTT = row['T_LuongTT']
-    Tien_Da_B = row['Gia_Ban1']
-    Tien_Da_BN = row['Gia_Ban2']
-    Tien_C_B = row['Gia_Ban5']
-    Tien_C_BN = row['Gia_Ban6']
-    Tien_Da_M = row['Gia_Ban3']
-    Tien_Da_MN = row['Gia_Ban4']
-    Tien_C_M = row['Gia_Ban7']
-    Tien_C_MN = row['Gia_Ban8']
-    Gia_B_TG = row['Gia_Ban9']
-    Gia_B_TGN = row['Gia_Ban10']
-    Gia_M_TG = row['Gia_Ban11']
-    Gia_M_TGN = row['Gia_Ban12']
-    TyGia_Ban = tygia['ty_gia_vang_ban_niem_yet']
-    TG_Mua_NY = tygia['ty_gia_vang_mua_niem_yet']
-    Tygia_Mua = tygia['ty_gia_vang_mua_niem_yet']
-    TyGia_TT = tygia['ty_gia_tien_te_ban_niem_yet']
-    TyGiaV_TG = row['Sl_Min']
-    TyGia_MuaN = row['Luu_Kho']
-    Hao_G_Cong = row['Tyle_GBL']
-    # Ma_QTTG = qttg_row['Ma_QTTG']
-    Ma_QTTG = tygia['ma_quy_tac_tinh_gia']
-    # He_So1 = qttg_row['He_So1']
-    He_So1 = tygia['he_so_1']
-    # He_So2 = qttg_row['He_So2']
-    He_So2 = tygia['he_so_2']
-    # He_So3 = qttg_row['He_So3']
-    He_So3 = tygia['he_so_3']
-    # He_So4 = qttg_row['He_So4']
-    He_So4 = tygia['he_so_4']
-    # He_So5 = qttg_row['He_So5']
-    He_So5 = tygia['he_so_5']
-    # He_So6 = qttg_row['He_So6']
-    He_So6 = tygia['he_so_6']
+    # TL_KLoai = row['T_Luong']
+    # TL_Da_C = row['The_Tich']
+    # T_LuongTT = row['T_LuongTT']
+    # Tien_Da_B = row['Gia_Ban1']
+    # Tien_Da_BN = row['Gia_Ban2']
+    # Tien_C_B = row['Gia_Ban5']
+    # Tien_C_BN = row['Gia_Ban6']
+    # Tien_Da_M = row['Gia_Ban3']
+    # Tien_Da_MN = row['Gia_Ban4']
+    # Tien_C_M = row['Gia_Ban7']
+    # Tien_C_MN = row['Gia_Ban8']
+    # Gia_B_TG = row['Gia_Ban9']
+    # Gia_B_TGN = row['Gia_Ban10']
+    # Gia_M_TG = row['Gia_Ban11']
+    # Gia_M_TGN = row['Gia_Ban12']
+    # TyGia_Ban = tygia['ty_gia_vang_ban_niem_yet']
+    # TG_Mua_NY = tygia['ty_gia_vang_mua_niem_yet']
+    # Tygia_Mua = tygia['ty_gia_vang_mua_niem_yet']
+    # TyGia_TT = tygia['ty_gia_tien_te_ban_niem_yet']
 
-    Tong_TL = TL_KLoai + TL_Da_C + T_LuongTT
+    # Ma_QTTG = qttg_row['Ma_QTTG']
+    # Ma_QTTG = tygia['ma_quy_tac_tinh_gia']
+    # # He_So1 = qttg_row['He_So1']
+    # He_So1 = tygia['he_so_1']
+    # # He_So2 = qttg_row['He_So2']
+    # He_So2 = tygia['he_so_2']
+    # # He_So3 = qttg_row['He_So3']
+    # He_So3 = tygia['he_so_3']
+    # # He_So4 = qttg_row['He_So4']
+    # He_So4 = tygia['he_so_4']
+    # # He_So5 = qttg_row['He_So5']
+    # He_So5 = tygia['he_so_5']
+    # # He_So6 = qttg_row['He_So6']
+    # He_So6 = tygia['he_so_6']
+
+    # Tong_TL = TL_KLoai + TL_Da_C + T_LuongTT
 
     # Logic tính giá theo Ma_QTTG
-    if Ma_QTTG in ['GB-10K-ORD', 'GB-14K-ORD', 'GB-18K-ORD']:
-        result = He_So1 * He_So2 * TyGia_Ban * TL_KLoai + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG == 'GB-24VD':
-        result = TL_KLoai * TyGia_Ban + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG == 'GB-BA-TG':
-        result = Gia_B_TG + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG == 'GB-BAC':
-        result = TL_KLoai * TyGia_Ban + Tien_C_B + Tien_Da_B
-    elif Ma_QTTG.startswith('GB-CN-CC') or Ma_QTTG == 'GB-CN-PT':
-        result = Tong_TL * (TyGia_Ban + He_So1 * He_So2) + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG == 'GB-CN-TG':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT
-    elif Ma_QTTG == 'GB-KGB':
-        result = TL_KLoai * TyGia_Ban
-    elif Ma_QTTG.startswith('GB-NC-1'):
-        result = Tong_TL * (TyGiaV_TG + He_So1)
-    elif Ma_QTTG.startswith('GB-NC-CC') or Ma_QTTG == 'GB-NC-PT':
-        result = Tong_TL * (TyGia_Ban + He_So1 * He_So2) + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG.startswith('GB-NCKC-1'):
-        result = He_So1 * (TL_KLoai * TyGiaV_TG + Tien_C_M + Tien_C_MN * TyGia_MuaN + Tien_Da_M + Tien_Da_MN * TyGia_MuaN)
-    elif Ma_QTTG.startswith('GB-NH-1'):
-        result = Tong_TL * (TyGiaV_TG + He_So1)
-    elif Ma_QTTG.startswith('GB-PH-1'):
-        result = TyGiaV_TG * Tong_TL * He_So1
-    elif Ma_QTTG == 'GB-PH-TG':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT
-    elif Ma_QTTG == 'GB-PT':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT
-    elif Ma_QTTG.startswith('GB-PY-1'):
-        result = Tong_TL * (TyGiaV_TG + He_So1)
-    elif Ma_QTTG.startswith('GB-PY-CC') or Ma_QTTG == 'GB-PY-PT':
-        result = Tong_TL * (TyGia_Ban + He_So1 * He_So2)
-    elif Ma_QTTG == 'GB-SJC':
-        result = TL_KLoai * TyGia_Ban
-    elif Ma_QTTG == 'GB-TK-TG':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT + Tien_Da_B + Tien_Da_BN * TyGia_TT
-    elif Ma_QTTG == 'GB-VRTL':
-        result = TL_KLoai * TyGia_Ban
-    elif Ma_QTTG.startswith('GB-VT-1'):
-        result = He_So1 * (TL_KLoai * He_So2 * TyGiaV_TG + Tien_Da_M + (Tien_C_M + TL_KLoai * Hao_G_Cong * Tygia_Mua / He_So2)) + He_So4
-    elif Ma_QTTG == 'GB-VT-TG':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT
-    elif Ma_QTTG == 'TRONGOI-GB':
-        result = Gia_B_TG + Gia_B_TGN * TyGia_TT
-    elif Ma_QTTG == 'TTXVV24K-GIABAN':
-        result = TL_KLoai * TyGia_Ban
-    else:
-        result = 0
+    # if Ma_QTTG in ['GB-10K-ORD', 'GB-14K-ORD', 'GB-18K-ORD']:
+    #     result = He_So1 * He_So2 * TyGia_Ban * TL_KLoai + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG == 'GB-24VD':
+    #     result = TL_KLoai * TyGia_Ban + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG == 'GB-BA-TG':
+    #     result = Gia_B_TG + Tien_C_B + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG == 'GB-BAC':
+    #     result = TL_KLoai * TyGia_Ban + Tien_C_B + Tien_Da_B
+    # elif Ma_QTTG.startswith('GB-CN-CC') or Ma_QTTG == 'GB-CN-PT':
+    #     result = Tong_TL * (TyGia_Ban + He_So1 * He_So2) + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG == 'GB-CN-TG':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT
+    # elif Ma_QTTG == 'GB-KGB':
+    #     result = TL_KLoai * TyGia_Ban
+    # elif Ma_QTTG.startswith('GB-NC-1'):
+    #     result = Tong_TL * (TyGiaV_TG + He_So1)
+    # elif Ma_QTTG.startswith('GB-NC-CC') or Ma_QTTG == 'GB-NC-PT':
+    #     result = Tong_TL * (TyGia_Ban + He_So1 * He_So2) + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG.startswith('GB-NCKC-1'):
+    #     result = He_So1 * (TL_KLoai * TyGiaV_TG + Tien_C_M + Tien_C_MN * TyGia_MuaN + Tien_Da_M + Tien_Da_MN * TyGia_MuaN)
+    # elif Ma_QTTG.startswith('GB-NH-1'):
+    #     result = Tong_TL * (TyGiaV_TG + He_So1)
+    # elif Ma_QTTG.startswith('GB-PH-1'):
+    #     result = TyGiaV_TG * Tong_TL * He_So1
+    # elif Ma_QTTG == 'GB-PH-TG':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT
+    # elif Ma_QTTG == 'GB-PT':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT
+    # elif Ma_QTTG.startswith('GB-PY-1'):
+    #     result = Tong_TL * (TyGiaV_TG + He_So1)
+    # elif Ma_QTTG.startswith('GB-PY-CC') or Ma_QTTG == 'GB-PY-PT':
+    #     result = Tong_TL * (TyGia_Ban + He_So1 * He_So2)
+    # elif Ma_QTTG == 'GB-SJC':
+    #     result = TL_KLoai * TyGia_Ban
+    # elif Ma_QTTG == 'GB-TK-TG':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT + Tien_Da_B + Tien_Da_BN * TyGia_TT
+    # elif Ma_QTTG == 'GB-VRTL':
+    #     result = TL_KLoai * TyGia_Ban
+    # elif Ma_QTTG.startswith('GB-VT-1'):
+    #     result = He_So1 * (TL_KLoai * He_So2 * TyGiaV_TG + Tien_Da_M + (Tien_C_M + TL_KLoai * Hao_G_Cong * Tygia_Mua / He_So2)) + He_So4
+    # elif Ma_QTTG == 'GB-VT-TG':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT
+    # elif Ma_QTTG == 'TRONGOI-GB':
+    #     result = Gia_B_TG + Gia_B_TGN * TyGia_TT
+    # elif Ma_QTTG == 'TTXVV24K-GIABAN':
+    #     result = TL_KLoai * TyGia_Ban
+    # else:
+    #     result = 0
 
-    return round(result, -3), tygia
+    return  tygia
 
 class PriceCalcView(APIView):
     def get(self, request):
@@ -510,21 +508,21 @@ class PriceCalcView(APIView):
 
         try:
             # Tìm sản phẩm theo SKU
-            row = df_dmH[df_dmH["Ma_Hang"] == sku]
-            if row.empty:
-                # Tìm kiếm những row có Ma_Hang bắt đầu băng sku
-                row = df_dmH[df_dmH["Ma_Hang"].notna() & df_dmH["Ma_Hang"].astype(str).str.startswith(sku)]
+            # row = df_dmH[df_dmH["Ma_Hang"] == sku]
+            # if row.empty:
+            #     # Tìm kiếm những row có Ma_Hang bắt đầu băng sku
+            #     row = df_dmH[df_dmH["Ma_Hang"].notna() & df_dmH["Ma_Hang"].astype(str).str.startswith(sku)]
 
-                if row.empty:
-                    return Response({"status": 404, "msg": "Không tìm thấy sản phẩm"}, status=404)
-            for _index, r in row.iterrows():
-                try:
+            #     if row.empty:
+            #         return Response({"status": 404, "msg": "Không tìm thấy sản phẩm"}, status=404)
+            # for _index, r in row.iterrows():
+                # try:
                 
-                    gia_ban_v2, realtime_price = tinh_gia_ban(r)
-                    row = r
-                    break
-                except Exception as e:
-                    continue
+            realtime_price = tinh_gia_ban(sku)
+                    # row = r
+                    # break
+                # except Exception as e:
+                    # continue
             # Gọi API tỷ giá
             url = f"{BASE_URL}/getTyGia"
             headers = {
@@ -546,52 +544,11 @@ class PriceCalcView(APIView):
                 return Response({"status": 500, "msg": "Không tìm thấy tỷ giá Kim Gia Bảo"}, status=500)
 
             # Tính giá bán = trọng lượng × tỷ giá
-            trong_luong = row.get("T_Luong", 0.0)
-            if pd.isna(trong_luong):
-                trong_luong = 0.0
+            # trong_luong = row.get("T_Luong", 0.0)
+            # if pd.isna(trong_luong):
+            #     trong_luong = 0.0
 
-            gia_ban = round(trong_luong * ty_gia_ban, -3)
-            gia_mua = round(trong_luong * ty_gia_ban * 0.98, -3)
-
-            # Trả về dữ liệu theo cấu trúc yêu cầu
-            response_data = {
-                "id": int(row["ID"]),
-                "maSanPham": row["Ma_Hang"],
-                "tenSanPham": row["Ten_Hang"],
-                "maVach": int(row["Ma_Vach"]),
-                "donViTinh": "Chiếc",
-                "tenHang": row.get("Ten_Hang1", "") or "",
-                "maNhom": row.get("Ma_Tong", "") or "",
-                "nguonNhap": "Phòng Cung ứng",
-                "ngayNhap": str(row["Ngay_Nhap"]) if not pd.isna(row["Ngay_Nhap"]) else None,
-                "quyCach": row.get("Qui_Cach", "") or "",
-                "inTrenHoaDon": row["Ten_Hang"],
-                "trongLuongKimLoai": trong_luong,
-                "trongLuongDaChinh": 0.0,
-                "trongLuongDaPhu": 0.0,
-                "trongLuongTem": 0.0,
-                "tienDaBan": 0.0,
-                "tienDaBanUsd": 0.0,
-                "tienCongBan": 0.0,
-                "tienCongBanUsd": 0.0,
-                "giaBan": gia_ban,
-                "nhaCungCap": "CTY",
-                "soLo": "",
-                "baoHanhHanSuDung": None,
-                "tieuChuan": "",
-                "xuatXu": "",
-                "soSeri": "",
-                "kich_co": "",
-                "ma_tong": row.get("Ma_Tong", "") or "",
-                "encodedString": None,
-                "giamua": gia_mua,
-                "ton_kho": 0,
-                "hamLuongKL": "24K",
-                "t_Luong": trong_luong,
-                "mo_Ta1": row.get("Mo_Ta", "") or "",
-                "trongluong": trong_luong,
-                "code": code if code else "dummy_code"
-            }
+            
             # realtime_price['code'] = code if code else "dummy_code"
             # Làm sạch dữ liệu trước khi trả về
             # cleaned_data = sanitize_json_floats(response_data)
@@ -603,7 +560,7 @@ class PriceCalcView(APIView):
                 # "data": cleaned_data,
                 "rate": rate_data,
                 # "realtime_price": realtime_price,
-                "gia_ban_thamchieu": gia_ban_v2
+                # "gia_ban_thamchieu": gia_ban_v2
             })
 
         except Exception as e:
